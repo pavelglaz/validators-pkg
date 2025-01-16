be7ea7d05fabf8b7f2fc13bab249df8d46c29f76
Павел Глазков <pavel.glazkov@fabit.ru>
2025-01-16 15:19:44 +0300

    Init packege validators

---
+++ src/validator.ts
@@ -0,0 +1,58 @@
+import FastestValidator, { ValidationSchema } from 'fastest-validator'
+
+import { ErrorCode, ValidationError, ValidationErrorField } from '@pavelglaz/errors'
+
+import { Rule } from './interfaces/rule'
+import { BufferValidationRule, DateValidationRule, ObjectIdValidationRule, PhoneNumberValidationRule, VersionValidationRule } from './rules'
+import { InternationalPhoneNumberValidationRule } from './rules/internationalPhoneNumber'
+
+export class AppValidator {
+    private readonly validator: FastestValidator
+
+    private customRules: Rule[]
+
+    constructor() {
+        this.customRules = [
+            new BufferValidationRule(),
+            new DateValidationRule(),
+            new ObjectIdValidationRule(),
+            new PhoneNumberValidationRule(),
+            new InternationalPhoneNumberValidationRule(),
+            new VersionValidationRule(),
+        ]
+
+        this.validator = new FastestValidator({ messages: this.getCustomValidatorsMessages() })
+
+        this.addCustomValidationRules()
+    }
+
+    compile(schema: ValidationSchema): (params: unknown) => boolean {
+        return (params: unknown): boolean => this.validate(params, schema)
+    }
+
+    validate(params: unknown, schema: ValidationSchema): boolean {
+        const res = this.validator.validate(params as Record<string, unknown>, schema) as true | ValidationErrorField[]
+
+        if (res !== true) {
+            throw new ValidationError(res, ErrorCode.ValidationError)
+        }
+
+        return true
+    }
+
+    private getCustomValidatorsMessages(): Record<string, string> {
+        const res: Record<string, string> = {}
+
+        for (const rule of this.customRules) {
+            res[rule.getName()] = rule.getMessage()
+        }
+
+        return res
+    }
+
+    private addCustomValidationRules(): void {
+        for (const rule of this.customRules) {
+            this.validator.add(rule.getName(), rule.getRule(this.validator))
+        }
+    }
+}
